818f464fb9b398278f0703d4e8f8a5ca49f0d439
Tejasri Baddam <tejasribaddam@981.com>
2025-03-02 13:23:44 -0500

    Frontend takequiz changes

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from '../signup/signup.component';
 import {DashboardComponent} from '../dashboard/dashboard.component'
+import { TakeQuizComponent } from '../takequiz/takequiz.component';
 
 const routes: Routes = [
   { path: 'login', loadChildren: () => import('../login/login.module').then(m => m.LoginModule) },
   { path: 'signup', component: SignupComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: ':category/:subcategory/:quizTopic/takequiz', component: TakeQuizComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
 
