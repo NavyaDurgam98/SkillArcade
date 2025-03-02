6ddd01e7df61ef1c33fd47292c2ba4d95fe1c9ad
sricharan2107 <sricharan210701@gmail.com>
2025-03-02 13:31:11 -0500

    Dashboard Implementation

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from '../signup/signup.component';
 import {DashboardComponent} from '../dashboard/dashboard.component'
+import { CategoryComponent } from '../category/category.component';
+import { QuizComponent } from '../quiz/quiz.component';
 
 const routes: Routes = [
   { path: 'login', loadChildren: () => import('../login/login.module').then(m => m.LoginModule) },
   { path: 'signup', component: SignupComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: ':category', component: CategoryComponent } ,
+  { path: ':category/:sub_category', component: QuizComponent },
+
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
 
@@ -15,3 +20,4 @@ const routes: Routes = [
   exports: [RouterModule],
 })
 export class AppRoutingModule { }
+
--- Frontend/src/quiz/quiz.module.ts
+++ Frontend/src/quiz/quiz.module.ts
@@ -1,16 +1,16 @@
-
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { DashboardComponent } from './dashboard.component';
+import { QuizComponent} from './quiz.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [QuizComponent],
   imports: [
     CommonModule,
     RouterModule 
   ],
-  exports: [DashboardComponent]
+  exports: [QuizComponent]
 })
 
 export class DashboardModule { }
+ 
