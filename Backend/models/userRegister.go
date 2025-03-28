6a8429528ad89fa31ac4aa9f7e510ce3a1053f95
sricharan2107 <sricharan210701@gmail.com>
2025-03-28 14:06:53 -0400

    UserId Implementation

--- Backend/models/userRegister.go
+++ Backend/models/userRegister.go
@@ -1,11 +1,14 @@
 package models
 
-type UserRegister struct{
-	FirstName string `json:"firstname" binding:"required"`
-	LastName string `json:"lastname" binding:"required"`
-	Email    string `json:"email" binding:"required"`
-	DOB 	 string `json:"dob"`
-	Gender	 string `json:"gender"`
-	Username string `json:"username" binding:"required"`
-	Password string `json:"password" binding:"required,min=6"`
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type UserRegister struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	FirstName string             `json:"firstname" binding:"required"`
+	LastName  string             `json:"lastname" binding:"required"`
+	Email     string             `json:"email" binding:"required"`
+	DOB       string             `json:"dob"`
+	Gender    string             `json:"gender"`
+	Username  string             `json:"username" binding:"required"`
+	Password  string             `json:"password" binding:"required,min=6"`
 }
--- Backend/services/UserLoginService.go
+++ Backend/services/UserLoginService.go
@@ -8,9 +8,10 @@ import (
 	"errors"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func UserLoginService(c context.Context, user *models.UserLogin) (string, error) {
+func UserLoginService(c context.Context, user *models.UserLogin) (string, string, error) {
 
 	// Access MongoDB collection
 	userDetailsCollection := Data.GetCollection("SkillArcade", "UserDetails")
@@ -19,21 +20,62 @@ func UserLoginService(c context.Context, user *models.UserLogin) (string, error)
 	var userExists bson.M
 	err := userDetailsCollection.FindOne(c, bson.M{"username": user.Username}).Decode(&userExists)
 	if err != nil {
-		//c.JSON(http.StatusUnauthorized, gin.H{"error": "Email not found"})
-		return user.Username, errors.New("user not found")
+		return user.Username, "", errors.New("user not found")
 	}
 
 	// Check if the passwords match
 	if userExists["password"] != user.Password {
-		//c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid password"})
-		return user.Username, errors.New("invalid password")
+		return user.Username, "", errors.New("invalid password")
 	}
+
+	// Extract ObjectID
+	userID := userExists["_id"].(primitive.ObjectID).Hex()
+
 	// Generate JWT token
 	token, err := utils.GenerateJWT(user.Username, user.Username)
 	if err != nil {
-		return "", err
+		return "", "", err
 	}
 
-	return token, nil
-	//return user.Username, nil
+	return token, userID, nil
 }
+
+// package services
+
+// import (
+// 	"BACKEND/Data"
+// 	"BACKEND/models"
+// 	"BACKEND/utils"
+// 	"context"
+// 	"errors"
+
+// 	"go.mongodb.org/mongo-driver/bson"
+// )
+
+// func UserLoginService(c context.Context, user *models.UserLogin) (string, error) {
+
+// 	// Access MongoDB collection
+// 	userDetailsCollection := Data.GetCollection("SkillArcade", "UserDetails")
+
+// 	// check if user exists in DB
+// 	var userExists bson.M
+// 	err := userDetailsCollection.FindOne(c, bson.M{"username": user.Username}).Decode(&userExists)
+// 	if err != nil {
+// 		//c.JSON(http.StatusUnauthorized, gin.H{"error": "Email not found"})
+// 		return user.Username, errors.New("user not found")
+// 	}
+
+// 	// Check if the passwords match
+// 	if userExists["password"] != user.Password {
+// 		//c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid password"})
+// 		return user.Username, errors.New("invalid password")
+// 	}
+// 	// Generate JWT token
+// 	token, err := utils.GenerateJWT(user.Username, user.Username)
+// 	if err != nil {
+// 		return "", err
+// 	}
+
+// 	return token, nil
+// 	//return user.Username, nil
+// }
