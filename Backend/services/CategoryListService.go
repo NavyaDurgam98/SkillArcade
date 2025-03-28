798e170767b26f23926b5ddbed19b47237d77d7f
sricharan2107 <sricharan210701@gmail.com>
2025-03-28 17:13:57 -0400

    category Search

--- Backend/services/CategoryListService.go
+++ Backend/services/CategoryListService.go
@@ -6,8 +6,8 @@ import (
 	"fmt"
 
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type CategoryList struct {
@@ -15,14 +15,23 @@ type CategoryList struct {
 	ImgPath      string `json:"imgPath" bson:"imgPath"`
 }
 
-func FetchCategories(c context.Context,collection *mongo.Collection) ([]CategoryList, error) {
-	// collection := Data.GetCollection("SkillArcade", "Quizzes")
+func FetchCategories(c context.Context, collection *mongo.Collection, searchText string) ([]CategoryList, error) {
+	// Create filter based on searchText
 	filter := bson.M{}
+	if len(searchText) >= 3 {
+		filter = bson.M{
+			"category": bson.M{
+				"$regex":   searchText,
+				"$options": "i", // case-insensitive
+			},
+		}
+	}
+
 	findOptions := options.Find()
 	findOptions.SetProjection(bson.M{
-		"category": 1, 
+		"category": 1,
 		"imgPath":  1,
-		"_id":      0, 
+		"_id":      0,
 	})
 
 	cursor, err := collection.Find(c, filter, findOptions)
@@ -38,3 +47,27 @@ func FetchCategories(c context.Context,collection *mongo.Collection) ([]Category
 
 	return categories, nil
 }
+
+// func FetchCategories(c context.Context,collection *mongo.Collection) ([]CategoryList, error) {
+// 	// collection := Data.GetCollection("SkillArcade", "Quizzes")
+// 	filter := bson.M{}
+// 	findOptions := options.Find()
+// 	findOptions.SetProjection(bson.M{
+// 		"category": 1,
+// 		"imgPath":  1,
+// 		"_id":      0,
+// 	})
+
+// 	cursor, err := collection.Find(c, filter, findOptions)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("error fetching categories: %v", err)
+// 	}
+// 	defer cursor.Close(c)
+
+// 	var categories []CategoryList
+// 	if err = cursor.All(c, &categories); err != nil {
+// 		return nil, fmt.Errorf("error decoding categories: %v", err)
+// 	}
+
+// 	return categories, nil
+// }
