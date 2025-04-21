68c2079ac33cced7f5a16db1f652dbc8f937fcbc
NavyaDurgam98 <165209687+NavyaDurgam98@users.noreply.github.com>
2025-04-21 13:44:51 -0400

    Update leaderboard.service.ts

--- Frontend/src/leaderboard/leaderboard.service.ts
+++ Frontend/src/leaderboard/leaderboard.service.ts
@@ -7,7 +7,7 @@ import { catchError } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class LeaderboardService {
-  private apiUrl = 'http://localhost:8080/leaderboard'; // Base URL
+  private apiUrl = `${environment.protectedApiUrl}/leaderboard`
 
   constructor(private http: HttpClient) {}
 
